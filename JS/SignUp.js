40346c50719d6e062b80c3491ff39543f0f84283
jmusser28-psu <jmusser28@psu.edu>
2026-02-20 00:20:16 -0500

    Works as expected now

--- JS/SignUp.js
+++ JS/SignUp.js
@@ -1,45 +1,37 @@
 function nameHandling(name) {
-   let valid = false;
-
-
    if (name.length == 0) {
-      lengthValid = false;
-      throw "Please enter a name";
+      return false;
    }
 
    for (let i = 0; i < name.length; i++) {
-      if ((name.charAt(i) >= 65 && name.charAt(i) <= 90) || !(name.charAt(i) >= 97 && name.charAt(i) <= 122)) {
-         valid = true;
-      }
-      else {
-        valid = false;
-        throw `invalid character: ${name.charA(i)}`;
-      }
+      if ((name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90) || (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122)) {}
+      else {return false;}
    }
-}
 
+   return true;
+}
 
 function emailHandling(email) {
-    let valid = false;
+   if (email.length == 0) { return false }
 
-    if (email.length == 0) {
-        valid = false;
-        throw "Please enter a name";
-    }
+   if (email.includes('@')) { }
+   else { return false }
 
-    for (let i = 0; i < email.length; i++) {
-      if ((email.charCodeAt(i) >= 64 && email.charCodeAt(i) <= 90) || (email.charCodeAt(i) >= 97 && email.charCodeAt(i) <= 122) || (email.charCodeAt(i) == 46)) {
-         valid = true;
+   let atCount = 0;
+   for (let i = 0; i < email.length; i++) {
+      if (email.charCodeAt(i) == 64) {
+         atCount = atCount + 1;
       }
    }
+   if (atCount > 1) { return false }
+
+   if (email.includes('.')) { }
+   else { return false }
 
    for (let i = 0; i < email.length; i++) {
-      if (email.charCodeAt(i) == 46) {
-         valid = true;
-      }
+      if ((email.charCodeAt(i) >= 64 && email.charCodeAt(i) <= 90) || (email.charCodeAt(i) >= 97 && email.charCodeAt(i) <= 122) || (email.charCodeAt(i) >= 48 && email.charCodeAt(i) <= 57) || (email.charCodeAt(i) == 46)) { }
+      else { return false }
    }
 
-    console.log(valid);
+   return true;
 }
-
-emailHandling("jmusser28@psu.edu");
