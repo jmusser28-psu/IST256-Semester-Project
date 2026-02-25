9109db6b44311ecb974c8a4247a59c5dd3e7bf36
Landen <landenmp4@gmail.com>
2026-02-25 11:21:34 -0500

    test

---
+++ JS/SignUp.js
@@ -0,0 +1,48 @@
+function nameHandling(name) {
+   if (name.length == 0) {
+      return false;
+   }
+
+   for (let i = 0; i < name.length; i++) {
+      if ((name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90) || (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122)) {}
+      else {return false;}
+   }
+
+   return true;
+}
+
+function emailHandling(email) {
+   if (email.length == 0) {return false}
+
+   if (email.includes('@')) {}
+   else {return false}
+
+   let atCount = 0;
+   for (let i = 0; i < email.length; i++) {
+      if (email.charCodeAt(i) == 64) {
+         atCount = atCount + 1;
+      }
+   }
+   if (atCount > 1) {return false}
+
+   if (email.includes('.')) {}
+   else { return false }
+
+   for (let i = 0; i < email.length; i++) {
+      if ((email.charCodeAt(i) >= 64 && email.charCodeAt(i) <= 90) || (email.charCodeAt(i) >= 97 && email.charCodeAt(i) <= 122) || (email.charCodeAt(i) >= 48 && email.charCodeAt(i) <= 57) || (email.charCodeAt(i) == 46)) {}
+      else {return false}
+   }
+
+   return true;
+}
+function phoneHandling(input_phone) {
+   let valid = false; 
+
+   if (input_phone == 0) {
+      lengthValid = false;
+      throw "Please enter a phone number"
+   }
+}
+
+emailHandling("jmusser28@psu.edu");
+
