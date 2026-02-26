87b64e798d6ef75a509b72dad307da204029cecf
jmusser28-psu <jmusser28@psu.edu>
2026-02-26 18:11:24 -0500

    validation.js does validation, it just needs to output to JSON now.

--- JS/validation.js
+++ JS/validation.js
@@ -4,7 +4,7 @@ function nameHandling(name) {
    }
 
    for (let i = 0; i < name.length; i++) {
-      if ((name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90) || (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122)) {}
+      if ((name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90) || (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122) || (name.charCodeAt(i) == 32)) {}
       else {return false;}
    }
 
@@ -12,10 +12,10 @@ function nameHandling(name) {
 }
 
 function emailHandling(email) {
-   if (email.length == 0) {return false}
+   if (email.length == 0) {return false;}
 
    if (email.includes('@')) {}
-   else {return false}
+   else {return false;}
 
    let atCount = 0;
    for (let i = 0; i < email.length; i++) {
@@ -23,14 +23,58 @@ function emailHandling(email) {
          atCount = atCount + 1;
       }
    }
-   if (atCount > 1) {return false}
+   if (atCount > 1) {return false;}
 
    if (email.includes('.')) {}
-   else { return false }
+   else {return false;}
 
    for (let i = 0; i < email.length; i++) {
       if ((email.charCodeAt(i) >= 64 && email.charCodeAt(i) <= 90) || (email.charCodeAt(i) >= 97 && email.charCodeAt(i) <= 122) || (email.charCodeAt(i) >= 48 && email.charCodeAt(i) <= 57) || (email.charCodeAt(i) == 46)) {}
-      else {return false}
+      else {return false;}
+   }
+
+   return true;
+}
+
+function phoneHandling(phoneNumber) {
+   let phoneExp = /^\d{3}-\d{3}-\d{4}$/;
+   if (phoneExp.test(phoneNumber) == true) {}
+   else {
+      return false;
+   }
+
+   return true;
+}
+
+function validate() {
+   let email = document.forms["signupForm"]["email"].value;
+   let name = document.forms["signupForm"]["name"].value;
+   let phoneNumber = document.forms["signupForm"]["phone"].value;
+   let passwordFirst = document.forms["signupForm"]["password"].value;
+   let passwordSecond = document.forms["signupForm"]["passwordConfirm"].value;
+   console.log(`${email}`);
+
+   if (nameHandling(name) == true) {}
+   else {
+      alert(`Invalid name: ${name}`);
+      return false;
+   }
+
+   if (emailHandling(email) == true) {}
+   else {
+      alert(`Invalid email: ${email}`);
+      return false;
+   }
+
+   if (phoneHandling(phoneNumber) == true) {}
+   else {
+      alert(`Invalid phone number: ${phoneNumber}`);
+      return false;
+   }
+
+   if ((passwordFirst.length > 12 && passwordFirst === passwordSecond) == false) {
+      alert(`Invalid password length or passwords do not match`);
+      return false;
    }
 
    return true;
