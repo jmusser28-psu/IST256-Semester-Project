e1c63012708eacc5fa0234c548a1dfdd72301cf5
jmusser28-psu <jmusser28@psu.edu>
2026-04-08 18:13:03 -0400

    Meeting Landen Jacob

--- JS/userStatus.js
+++ JS/userStatus.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
     let user = JSON.parse(localStorage.getItem('user'));
 
-    if (user === null) {}
+    if (user === null) {
+        $(".notLoggedIn").attr("hidden", true);
+        $(".subscriber").html("Login to view more options")
+    }
     else {
         $("#management").html(`
                         <option selected disabled>${user}</option>
+                        <option>Sign Out</option>
                         `)
         $("#management").attr("value", user);
+        $("#management").addClass("signoutClass", true);
     }
+})
+
+$(document).on("change", ".signoutClass", function(event) {
+    event.preventDefault();
+    localStorage.clear("user");
+    alert("Successfully signed out");
+    location.reload();
 })
